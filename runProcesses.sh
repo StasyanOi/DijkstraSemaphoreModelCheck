fd749ba5d91d533f5f3c98907bfb7c6d414d6e82
Stas Tarasov <iluvstrwrs@mail.ru>
2020-11-28 00:46:15 +0300

    refactor

--- runProcesses.sh
+++
@@ -1,8 +0,0 @@
-clear
-cat ./code/processes.pml > ./code/verify.pml
-spin ./code/verify.pml
-rm ./code/verify.pml
-rm pan
-rm pan.*
-rm *.tmp
-rm *.trail
--- runVerify.sh
+++
@@ -1,6 +0,0 @@
-spin -a ./code/processes.pml
-gcc -o pan pan.c
-./pan -a -N $1
-mv ./processes.pml.trail ./code/processes.pml.trail
-rm ./code/processes.pml.trail
-spin -t -p ./code/processes.pml
