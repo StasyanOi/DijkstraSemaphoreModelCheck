4aa8c8ab83c9aae7bb0115d1f26075146bfb175b
Stas Tarasov <iluvstrwrs@mail.ru>
2020-12-03 19:08:26 +0300

    fixes

--- Java/src/main/java/Main.java
+++ Java/src/main/java/Main.java
@@ -11,26 +11,27 @@ public class Main {
 
         for (int i = 1; i <= 5; i++) {
             for (int j = i + 1; j <= 5; j++) {
-                for (int k = j + 1; k <= 5; k++) {
-                    procLtl.add(new int[]{i, j, k});
-                }
+                //for (int k = j + 1; k <= 5; k++) {
+                    procLtl.add(new int[]{i, j});
+                //}
             }
         }
 //        (proc1.state = critical & proc2.state = critical & proc3.state = critical)
         String collect = procLtl.stream()
-                .map(ints -> " F (proc" + ints[0] + ".state = critical & proc" + ints[1] + ".state = critical & proc" + ints[2] + ".state = critical)")
+                .map(ints -> " F ((proc" + ints[0] + ".state = critical1 | proc " + "proc" + ints[0] + ".state = critical2)" +
+                        " & F (proc" + ints[1] + ".state = critical1 | " + "proc" + ints[1] + ".state = critical2)")
                 .collect(Collectors.joining(" \n| "));
 
-        String collect1 = procLtl.stream()
-                .map(ints -> " F (proc" + ints[0] + ".state = critical & proc" + ints[1] + ".state = critical & proc" + ints[2] + ".state != critical)")
-                .collect(Collectors.joining(" \n| "));
-
-        String collect2 = procLtl.stream()
-                .map(ints -> " F (proc" + ints[0] + ".state = critical & proc" + ints[1] + ".state != critical & proc" + ints[2] + ".state != critical)")
-                .collect(Collectors.joining(" \n| "));
+//        String collect1 = procLtl.stream()
+//                .map(ints -> " F (proc" + ints[0] + ".state = critical & proc" + ints[1] + ".state = critical & proc" + ints[2] + ".state != critical)")
+//                .collect(Collectors.joining(" \n| "));
+//
+//        String collect2 = procLtl.stream()
+//                .map(ints -> " F (proc" + ints[0] + ".state = critical & proc" + ints[1] + ".state != critical & proc" + ints[2] + ".state != critical)")
+//                .collect(Collectors.joining(" \n| "));
 
         System.out.println(collect);
-        System.out.println("| " + collect1);
-        System.out.println("| " + collect2);
+//        System.out.println("| " + collect1);
+//        System.out.println("| " + collect2);
     }
 }
